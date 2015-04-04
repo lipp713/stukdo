2e42f85565ea508a7c7175992dec3fc94f8ea87c
lipp713 <lipp713@gmail.com>
2015-04-04 01:56:12 +0100

    Added the About page

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Stukdo::Application.routes.draw do
-  root 'pages#home'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
+  root 'pages#home'
   
+  get 'about' => "pages#about"
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
